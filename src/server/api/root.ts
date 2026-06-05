a59a13261eb48972feaad0271cf57f1e6bde2c0b
kshwadip <277583349+kshwadip@users.noreply.github.com>
2026-06-05 23:21:27 +0530

    Revert "Phase 3 Implemented"
    This reverts commit 0bf639546566b32b2d637fb53b902aae97f68de0.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,12 +1,8 @@
 import { receiptsRouter } from "~/server/api/routers/receipts";
-import { profileRouter } from "~/server/api/routers/profile";
-import { analyticsRouter } from "~/server/api/routers/analytics";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 export const appRouter = createTRPCRouter({
   receipts: receiptsRouter,
-  profile: profileRouter,
-  analytics: analyticsRouter,
 });
 
 export type AppRouter = typeof appRouter;
